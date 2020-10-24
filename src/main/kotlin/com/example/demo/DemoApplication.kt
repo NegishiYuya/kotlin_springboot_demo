af551f72dee133ec54c3fb978c1cf0ec2c4ebe26
NegishiYuya <negishiyuya0115@gmail.com>
2020-10-24 21:39:27 +0900

    DIの設定追加

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,14 @@
 package com.example.demo
 
+import com.example.dbflute.allcommon.DBFluteBeansJavaConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(DBFluteBeansJavaConfig::class)
 class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
